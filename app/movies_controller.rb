e7c6b54492fbaeda38a042126dff0662b02ef247
Tony Baker <this.tonybaker@gmail.com>
2019-05-29 23:43:59 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -74,7 +74,8 @@ end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
-  Movie.create(title: "Wat?")
+  movie = Movie.create(title: "Wat?")
+  movie.update(title: "Wat, huh?")
   __
   __
 end
