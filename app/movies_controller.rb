9864767a84eb8af49867321b9fcd0385238b82b8
Tony Baker <this.tonybaker@gmail.com>
2019-05-29 23:59:29 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -88,8 +88,8 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
-  __
+  found_movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
+  found_movie.destroy
 end
 
 def can_destroy_all_items_at_once
